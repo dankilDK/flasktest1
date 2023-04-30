011b8861ae8f153714ca8f8910d57d240dcda283
dankilDK <dankilDK@yandex.ru>
2023-04-30 16:10:57 +0500

    task 1-9

--- main.py
+++ main.py
@@ -58,52 +58,48 @@ def ad():
                 </html>"""
 
 
-@app.route('/load_photo', methods=['POST', 'GET'])
-def selection():
-    if request.method == 'GET':
-        return f"""
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                <link rel="stylesheet"
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                crossorigin="anonymous">
-                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                <title>Отбор астровнавтов</title>
-            </head>
-            <body>
-                <h1>Загрузка фотографии</h1>
-                <h2>для участия в миссии</h2>
-                <div>
-                    <form class="login_form" method="post" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label for="photo">Приложите фотографию</label>
-                            <input type="file" class="form-control-file" id="photo" name="file">
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Пейзажи Марса</title>
+                </head>
+                <body>
+                    <h1>Пейзажи Марса</h1>
+                    <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExample" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExample" data-slide-to="1"></li>
+                            <li data-target="#carouselExample" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="{url_for('static', filename='img/landscape1.jpg')}" class="d-block w-100" alt="First Slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{url_for('static', filename='img/landscape2.jpg')}" class="d-block w-100" alt="Second Slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{url_for('static', filename='img/landscape3.jpg')}" class="d-block w-100" alt="Third Slide">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <img src="{url_for('static', filename='img/avatar.jpg')}" 
-                            alt="Здесь должно быть Ваше фото">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Отправить</button>
-                    </form>
-                </div>
-            </body>
-        </html>
-        """
-    elif request.method == "POST":
-        im = Image.open(request.files['file'])
-        try:
-            im.save("static/img/avatar.jpg")
-        except Exception:
-            im.load()
-            res = Image.new("RGB", im.size, (255, 255, 255))
-            res.paste(im, mask=im.split()[3])
-            res.save("static/img/avatar.jpg")
-        print(request.files['file'])
-        return "Форма отправлена"
+                        <a class="carousel-control-prev" role="button" href="#carouselExample" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        </a>
+                        <a class="carousel-control-next" role="button" href="#carouselExample" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </body>
+                </html>'''
 
 
 if __name__ == '__main__':
